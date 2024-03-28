5819e88b8ad06291db8d4d589808f12afb8a4f93
GuptaPraveenKumar <149958810+guptapraveen009@users.noreply.github.com>
2024-03-28 23:36:29 +0530

    Add files via upload

---
+++ practiceset4.js
@@ -0,0 +1,22 @@
+//chapter4//
+//qusetion 1
+let name="har\""
+console.log(name.length)
+
+//qusetion 2//
+let a="praveen kumar";
+console.log(a.startsWith("p"))
+console.log(a.endsWith("r"))
+
+//question no 3
+let b="HI,MY NAME IS PRAVEEN. I AM A BEGINNER IN WEB DEVLOPMENT ";
+console.log(b.toLocaleLowerCase())
+
+//question no 4//
+let amt="please give me rs 1000"
+console.log(amt.length)
+ console.log(amt.substring(15,23))
+
+ //question no 5
+let r="prapeen";
+console.log(r.replace("p","v"))
---
+++ strings.js
@@ -0,0 +1,19 @@
+// let name ="praveen";
+// console.log(name.length)
+//  //template litreal//
+// let a='praveen',b='sham'; console.log(`${a}\tis the bussiness patner of${b}`); //escape sequence//
+// let w="this my javascripit program.\r To perfrom escape sequence character in javascripit" 
+// console.log(w);
+
+// //strings methods
+// let s="harry";
+// console.log(name.length);
+// console.log(name.toUpperCase())
+// let friend="rahul";
+// console.log(name.concat("is the friend of"+friend,"\tok"))
+
+//using for loop add the strings
+let name ="praveen";
+for(let i=0;iname.length;i++){
+    console.log(name);
+}
