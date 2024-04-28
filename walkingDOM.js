f90ded47a802ab6309987f85fa394482dde2b050
GuptaPraveenKumar <149958810+guptapraveen009@users.noreply.github.com>
2024-04-28 17:33:49 +0530

    Update walkingDOM.js

--- walkingDOM.js
+++ walkingDOM.js
@@ -5,25 +5,6 @@
 // console.log(a.parentElement);
 // console.log(a.parentNode);
 // console.log(a.nextSibling);
-// const { first } = require("lodash");
 // const changeColor=()=>{
 //     document.body.firstElementChild.style.background="red"
 // }
-//  let a=first.className="text-drak red";
-// console.log(a)
-
-// alert("random boy");
-// setTimeout(function(){
-//     confirm("are you human")
-// } ,1000)
-
-// const sum=(a,b)=>{
-//     console.log("the sum of a+b",a+b)
-// }
-// setTimeout(sum,100,1,3);
-// clearTimeout(sum);
-
-// let setintreval=setInterval(function(){
-//     confirm("Do you want to play again")
-// },2000)
-// clearInterval(setintreval)
